38af9f91552364d89507ff0d9974bcfd79dd95c8
Abhijay007j <Abhijay007j@gmail.com>
2021-01-25 20:56:50 +0530

    improve UI and Remove unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,43 +1,41 @@
-import React, {useState} from "react";
-import CircularSlider from '@fseehawer/react-circular-slider';
-import  Upload from"./components/Upload.component";
-import  Compressor from"./components/Compressor.component";
-import  Header from"./components/Header.component";
+import React from "react";
+import Upload from "./components/Upload.component";
+import Compressor from "./components/Compressor.component";
+import Header from "./components/Header.component";
 import imageCompression from "browser-image-compression";
-import"./App.css";
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       compressedLink:
-        "https://www.flaticon.com/svg/static/icons/svg/3588/3588226.svg",
+      "https://www.flaticon.com/svg/static/icons/svg/3588/3588226.svg",
       originalImage: "",
-      outputImage:"",
+      outputImage: "",
       originalLink: "",
       clicked: false,
       uploadImage: false,
-      quality:0,
-      count:0
+      quality: 0,
+      count: 0
     };
-    
-  } 
-handleChange = value => {
+
+  }
+  handleChange = value => {
     this.setState({
-        quality: value,
+      quality: value,
     });
-};
+  };
   changeValue = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
-  showSettings (event) {
+  showSettings(event) {
     event.preventDefault();
   }
   click = e => {
     e.preventDefault();
 
     const options = {
-      maxSizeMB: this.state.quality/1024,
-      maxWidthOrHeight:this.state.originalImage.height,
+      maxSizeMB: this.state.quality / 1024,
+      maxWidthOrHeight: this.state.originalImage.height,
       useWebWorker: true
     };
 
@@ -45,14 +43,14 @@ handleChange = value => {
       alert("Bring a bigger image");
       return 0;
     }
-    
+
     let output;
     imageCompression(this.state.originalImage, options).then(x => {
       output = x;
       const downloadLink = URL.createObjectURL(output);
       this.setState({
         compressedLink: downloadLink,
-        outputImage:output,
+        outputImage: output,
       });
     });
 
@@ -60,47 +58,45 @@ handleChange = value => {
     return 1;
   };
   handle = e => {
- 
+
     const imageFile = e.target.files[0];
     this.setState({
       originalLink: URL.createObjectURL(imageFile),
       originalImage: imageFile,
       outputFileName: imageFile.name,
       uploadImage: true,
-      
+
     });
-   
-    
+
+
   };
-  render()
-  {  
-    let whole={
-      overflow:"hidden",
-      overflowY:"hidden",
+  render() {
+    let whole = {
+      overflow: "hidden",
+      overflowY: "hidden",
     }
-    return(
-     <div className="App" style={whole}>
-      <Header />
-      <div className="row mx-5 justify-content-center align-items-center y-5" >
-      <Upload uploadImage={this.state.uploadImage} 
-                       originalLink={this.state.originalLink}
-                       originalImage={this.state.originalImage} 
-                       handle={this.handle} />
+    return (
+      <div className="App" style={whole}>
+        <Header />
+        <div className="row mx-5  justify-content-center align-items-center y-5 s-5" >
+          <Upload uploadImage={this.state.uploadImage}
+            originalLink={this.state.originalLink}
+            originalImage={this.state.originalImage}
+            handle={this.handle} />
 
-      <Compressor     outputFileName={this.state.outputFileName}
-                       quality={this.state.quality} 
-                       handleInputChange={this.handleChange} 
-                       click={this.click} 
-                       compressedLink={this.state.compressedLink} 
-                       outputImage={this.state.outputImage} 
-                       clicked={this.state.clicked} 
-                       outputFileName={this.state.outputFileName} 
-                      />
-        </div>           
-        </div> 
-     );
-   }
- }
-
+          <Compressor outputFileName={this.state.outputFileName}
+            quality={this.state.quality}
+            handleInputChange={this.handleChange}
+            click={this.click}
+            compressedLink={this.state.compressedLink}
+            outputImage={this.state.outputImage}
+            clicked={this.state.clicked}
+            outputFileName={this.state.outputFileName}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/Upload.component.jsx
+++ src/components/Upload.component.jsx
@@ -1,56 +1,55 @@
 import React from "react";
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 import Card from "react-bootstrap/Card";
-var style={
-  backgroundColor: "#1a1c20",
-  
+var style = {
+  backgroundColor: "eee",
+
 }
-var up={
+var up = {
   height: "200px",
   width: "auto",
 }
-const Upload= (props)=>{
-    return (
-          
-            <div className="col-xl-3 col-lg-3 col-md-11 col-sm-11 mt-2 p-5 m-10 ht mt-4" style={style}>
-              {props.uploadImage ? (
-                <Card.Img
-                  id="icons"
-                  className="card-img-top ht mt-2"
-                  variant="top"
-                  src={props.originalLink}
-                  style={up}
-                ></Card.Img>
-              ) : (
-                <Card.Img
-                  id="icons"
-                  className="ht"
-                  variant="top"
-                  src="https://www.flaticon.com/svg/static/icons/svg/3588/3588235.svg"
-                ></Card.Img>
-              )}
-               {props.originalImage ? (
-                <div id="kb" className="d-flex ml-10 justify-content-center">
-                  <h5>{Number((parseInt(props.originalImage.size, 10)/1024).toFixed(1))} KB</h5>
-                </div>
-              ) : (
-                <></>
-              )} 
-              <div className="d-flex justify-content-center">
-                
-                <input
-                  type="file"
-                  accept="image/*"
-                  className="mt-2 btn btn-dark w-75"
-                  onChange={e => props.handle(e)}
-                />
-              </div>
-                
-            </div>
-  
-        );
+const Upload = (props) => {
+  return (
+
+    <div className="col-xl-3 col-lg-3 col-md-11 col-sm-11 mt-2 p-5 m-10 ht mt-4" style={style}>
+      {props.uploadImage ? (
+        <Card.Img
+          id="icons"
+          className="card-img-top ht mt-2"
+          variant="top"
+          src={props.originalLink}
+          style={up}
+        ></Card.Img>
+      ) : (
+          <Card.Img
+            id="icons"
+            className="ht"
+            variant="top"
+            src="https://media.giphy.com/media/vniHEov2M4XWJOU9dC/giphy.gif"
+          ></Card.Img>
+        )}
+      {props.originalImage ? (
+        <div id="kb" className="d-flex ml-10 justify-content-center">
+          <h5>{Number((parseInt(props.originalImage.size, 10) / 1024).toFixed(1))} KB</h5>
+        </div>
+      ) : (
+          <></>
+        )}
+      <div className="d-flex justify-content-center">
+
+        <input
+          type="file"
+          accept="image/*"
+          className="mt-2 btn btn-dark w-75"
+          onChange={e => props.handle(e)}
+        />
+      </div>
+
+    </div>
+
+  );
 }
 
 export default Upload;
